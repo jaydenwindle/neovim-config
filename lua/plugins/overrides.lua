5e2687ef361d42aa0c794e484c7495300e358565
Jayden Windle <jaydenwindle@gmail.com>
2024-12-28 13:13:23 -0800

    Added guess-indent and setup tab + enter completion

---
+++ lua/plugins/overrides.lua
@@ -0,0 +1,33 @@
+return {
+  -- super-tab + enter completion
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "super-tab",
+        ["<Tab>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        },
+        ["<Enter>"] = {
+          function(cmp)
+            if cmp.snippet_active() then
+              return cmp.accept()
+            else
+              return cmp.select_and_accept()
+            end
+          end,
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+          "fallback",
+        },
+      },
+    },
+  },
+}
